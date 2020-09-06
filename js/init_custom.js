15c284cb2cf6ac625215e8b61d2bca3039e6a75d
iganchev87 <iganchev87@gmail.com>
2020-09-06 11:37:43 +0300

    fix format problem

--- js/init_custom.js
+++ js/init_custom.js
@@ -1,30 +1,32 @@
-jQuery(document).ready(function($) {
+jQuery( document ).ready( function ( $ ) {
     const _g_calendar = 'https://calendar.google.com/calendar/event?action=TEMPLATE';
-    $('.g_calendar').datepicker({ dateFormat: 'yy-mm-dd' });
 
-    $('.gCalendarLink').on('click', function(ev) {
+    $( '.g_calendar' ).datepicker( { dateFormat: 'yy-mm-dd' } );
+
+    $( '.gCalendarLink' ).on( 'click', function ( ev ) {
         ev.preventDefault();
-        let url = setGoogleUrlCalendar($(ev.target).closest('.g_calendar_container'));
-        console.log(url);
-        window.open(url, "_blank");
-    });
+        let url = setGoogleUrlCalendar( $( ev.target ).closest( '.g_calendar_container' )  );
+        window.open( url, "_blank" );
+    } );
 
-    function setGoogleUrlCalendar($container) {
+    function setGoogleUrlCalendar ( $container ) {
         let _url = _g_calendar;
         let $gCalendarContainer = $container;
-        let ev_date = $gCalendarContainer.find('.g_calendar').first().datepicker('getDate');
-        let location = $gCalendarContainer.find('.g_location').val();
-        if (ev_date !== '') {
-            let gFormatDate = (ev_date.toISOString()).replace(/\-/g, "").replace(/\:/g, "").split('.')[0] + 'Z';
-            _url = _url + "&date=" + encodeURI(gFormatDate + "/" + gFormatDate);
-            _url = _url + "&dates=" + encodeURI(gFormatDate + "/" + gFormatDate);
+        let ev_date = $gCalendarContainer.find( '.g_calendar' ).first( ).datepicker( 'getDate' );
+        let location = $gCalendarContainer.find( '.g_location' ).val( );
+
+        if ( ev_date !== '' ) {
+            let gFormatDate = ( ev_date.toISOString() ).replace( /\-/g, "" ).replace( /\:/g, "" ).split( '.' ) [0] + 'Z';
+            _url = _url + "&date=" + encodeURI( gFormatDate + "/" + gFormatDate );
+            _url = _url + "&dates=" + encodeURI( gFormatDate + "/" + gFormatDate );
         }
-        if (location !== '') {
-            _url = _url + "&location=" + encodeURI(location);
+
+        if ( location !== '' ) {
+            _url = _url + "&location=" + encodeURI( location );
         }
-        $(this).closest('.g_calendar_container').find('.gCalendarLink').attr("href", _url)
+        $( this ).closest( '.g_calendar_container' ).find( '.gCalendarLink' ).attr( "href", _url ) 
         return _url;
     }
 
-    $('#post-body').find('#title').attr('required', 'required');
-});
+    $( '#post-body' ).find( '#title' ).attr( 'required', 'required' );
+} );
